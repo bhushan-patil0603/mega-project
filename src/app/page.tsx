bf2d2abfe8d7bb3da30dc1eaa2dad24b231db210
bhushan <pbhushan0603@gmail.com>
2025-03-29 21:09:35 +0530

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import { ThemeToggle } from "@/components/theme-toggle";
+import HomePage from "@/components/home-page";
 
 export default function Home() {
   return (
     <main
-      className={"relative flex h-screen flex-col items-center justify-center"}
+      className={""}
     >
-      <h1 className={"font-medium text-7xl tracking-tighter"}>
-        Next Essential
-      </h1>
-      <p className={"my-6 text-center text-base"}>
-        An Essential Template to get started with Next.js application with
-        essential.
-      </p>
-      <ThemeToggle />
+      <HomePage/>
     </main>
   );
 }
